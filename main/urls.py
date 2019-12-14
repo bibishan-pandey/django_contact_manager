aeba29b9a5b81ea71b3bc393d14365d1be4de6fa
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-12-14 12:16:26 +0545

    handling images for contact in media files

--- main/urls.py
+++ main/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('contacts.urls'))
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('contacts.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Contact Manager Administration'
 admin.site.index_title = 'Project administration'
